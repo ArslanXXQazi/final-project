d1aa902a58963f26c1f49c14980efa3d7c704c15
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-03 21:31:42 +0500

    Update main.dart

--- final_projrct/lib/main.dart
+++ final_projrct/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:final_projrct/src/views/insert_view/insert_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,7 +32,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: InsertView(),
     );
   }
 }
