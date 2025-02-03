febdac209fe5d41f6054b60fb482d0e2cf17d1db
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-03 21:35:48 +0500

    Update custom_Text.dart

--- final_projrct/lib/src/controller/custom_Text.dart
+++ final_projrct/lib/src/controller/custom_Text.dart
@@ -1,1 +1,21 @@
 import 'package:flutter/material.dart';
+
+class CustomText extends StatelessWidget {
+  String tex;
+  Color color;
+  FontWeight fontWeight;
+  double  fontSize;
+   CustomText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '',
+      style: TextStyle(
+        color: ,
+        fontSize: ,
+        fontWeight: ,
+      ),
+    )
+  }
+}
