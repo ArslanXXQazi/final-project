98aa50ee591cd28847b5cf6c6fc943bf88deb3ae
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-03 22:27:57 +0500

    Update custom_button.dart

--- final_projrct/lib/src/controller/components/custom_button.dart
+++ final_projrct/lib/src/controller/components/custom_button.dart
@@ -2,14 +2,16 @@ import 'package:final_projrct/src/controller/components/custom_Text.dart';
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+  VoidCallback onTap;
+  String name;
+   CustomButton({super.key,required this.onTap,required this.name});
 
   @override
   Widget build(BuildContext context) {
     final height=MediaQuery.sizeOf(context).height;
     final width=MediaQuery.sizeOf(context).width;
     return InkWell(
-      onTap: (){},
+      onTap: onTap,
       child: Container(
         height: height*.06,
         width: width*1,
@@ -17,7 +19,7 @@ class CustomButton extends StatelessWidget {
           color: Colors.orange,
           borderRadius: BorderRadius.circular(10),
         ),
-        child: Center(child: CustomText(text: 'Inter Data',color: Colors.white,fontSize: 20,)),
+        child: Center(child: CustomText(text: name,color: Colors.white,fontSize: 20,)),
       ),
     );
   }
