087e2365c82682d69baec45c081824a6c9568240
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-03 21:30:42 +0500

    Update insert_view.dart

---
+++ final_projrct/lib/src/views/insert_view/insert_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class InsertView extends StatefulWidget {
+  const InsertView({super.key});
+
+  @override
+  State<InsertView> createState() => _InsertViewState();
+}
+
+class _InsertViewState extends State<InsertView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
