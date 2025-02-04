b95c9edc538df4118357e4e922c7e3bc833e8795
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-04 12:17:22 +0500

    update

--- final_projrct/lib/src/views/home_views/fetch_view/fetch_view.dart
+++ final_projrct/lib/src/views/home_views/fetch_view/fetch_view.dart
@@ -28,6 +28,25 @@ class FetchView extends StatelessWidget {
             color: Colors.red,
             borderRadius: BorderRadius.circular(10),
           ),
+          child: Column(children: [
+            CustomText(text: "Which package is commonly used for state management in Flutter?"),
+            Row(children: [
+              CustomText(text: "1)"),
+              CustomText(text: "Provider"),
+            ],),
+            Row(children: [
+              CustomText(text: "2)"),
+              CustomText(text: "Riverpod"),
+            ],),
+            Row(children: [
+              CustomText(text: "3)"),
+              CustomText(text: "Bloc"),
+            ],),
+            Row(children: [
+              CustomText(text: "3)"),
+              CustomText(text: "GetX"),
+            ],),
+          ],),
         )
       ],),
     );
