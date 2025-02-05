b9adc5deb30126f3dc02ed0383bd51616abee202
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-05 22:15:53 +0500

    Update fetch_view.dart

--- final_projrct/lib/src/views/home_views/fetch_view/fetch_view.dart
+++ final_projrct/lib/src/views/home_views/fetch_view/fetch_view.dart
@@ -77,7 +77,7 @@ class _FetchViewState extends State<FetchView> {
            Container(
              height: height*.1,
              width: width*1,
-             child: CustomText(text: data[index].question??""),
+             child: CustomText(text: data[index].option3??""),
            ),
            SizedBox(height: height*.02,),
            Container(
