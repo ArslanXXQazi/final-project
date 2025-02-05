8985fb77cae88102b878d5fa26eb0a77ecb55f02
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-05 22:09:54 +0500

    update

--- final_projrct/lib/src/views/home_views/fetch_view/fetch_view.dart
+++ final_projrct/lib/src/views/home_views/fetch_view/fetch_view.dart
@@ -35,6 +35,7 @@ class _FetchViewState extends State<FetchView> {
         option4: noteMap['option4'],
       );
     }).toList();
+    setState(() {});
   }
 
   Widget build(BuildContext context) {
@@ -51,84 +52,7 @@ class _FetchViewState extends State<FetchView> {
           fontWeight: FontWeight.bold,
         ),
       ),
-      body: Padding(
-        padding:  EdgeInsets.symmetric(horizontal: width*.02,vertical: height*.02),
-        child: Column(
-          children: [
-          Container(
-            height: height*.3,
-            width: width*1,
-            decoration: BoxDecoration(
-              color: Colors.blue.shade50,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Padding(
-              padding:  EdgeInsets.symmetric(horizontal: width*.02),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                CustomText(text: "Which package is commonly used for state management in Flutter?"),
-                Row(children: [
-                 Container(
-                   height: height*.05,
-                   width: width*.05,
-                   decoration: BoxDecoration(
-                     color: Colors.white,
-                     shape: BoxShape.circle,
-                     border: Border.all(color: Colors.grey),
-                   ),
-                   child: Icon(Icons.check,size: width*.04,),
-                 ),
-                 SizedBox(width: width*.02,),
-                  CustomText(text: "Provider"),
-                ],),
-                Row(children: [
-                  Container(
-                    height: height*.05,
-                    width: width*.05,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.grey),
-                    ),
-                    child: Icon(Icons.check,size: width*.04,),
-                  ),
-                  SizedBox(width: width*.02,),
-                  CustomText(text: "Provider"),
-                ],),
-                Row(children: [
-                  Container(
-                    height: height*.05,
-                    width: width*.05,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.grey),
-                    ),
-                    child: Icon(Icons.check,size: width*.04,),
-                  ),
-                  SizedBox(width: width*.02,),
-                  CustomText(text: "Provider"),
-                ],),
-                Row(children: [
-                  Container(
-                    height: height*.05,
-                    width: width*.05,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.grey),
-                    ),
-                    child: Icon(Icons.check,size: width*.04,),
-                  ),
-                  SizedBox(width: width*.02,),
-                  CustomText(text: "Provider"),
-                ],),
-              ],),
-            ),
-          )
-        ],),
-      ),
+     
     );
   }
 }
