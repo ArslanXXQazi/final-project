d01a1f5ea527f94ea09017edebd6eb6e509b8f88
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-06 12:47:05 +0500

    Create home_view.dart

---
+++ final_projrct/lib/src/views/home_views/home_view/home_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final height=MediaQuery.sizeOf(context).height;
+    final width=MediaQuery.sizeOf(context).width;
+    return Scaffold(
+      body: Column(children: [
+        
+      ],),
+    );
+  }
+}
