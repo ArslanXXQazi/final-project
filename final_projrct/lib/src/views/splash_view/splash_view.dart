be416f8e7e5da64e3a280c69be20a961bb617af6
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-06 11:43:40 +0500

    update

---
+++ final_projrct/lib/src/views/splash_view/splash_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: Colors.black,
+      ),
+    );
+  }
+}
+
