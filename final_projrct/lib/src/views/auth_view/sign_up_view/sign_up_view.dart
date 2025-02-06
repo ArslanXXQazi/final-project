4ea07cc722ba91e22d1864f529a199ae1a2cb8f0
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-06 12:03:34 +0500

    Update sign_up_view.dart

--- final_projrct/lib/src/views/auth_view/sign_up_view/sign_up_view.dart
+++ final_projrct/lib/src/views/auth_view/sign_up_view/sign_up_view.dart
@@ -1,4 +1,5 @@
 import 'package:final_projrct/src/controller/components/custom_Text.dart';
+import 'package:final_projrct/src/controller/components/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 
 class SignUpView extends StatefulWidget {
@@ -9,6 +10,10 @@ class SignUpView extends StatefulWidget {
 }
 
 class _SignUpViewState extends State<SignUpView> {
+
+  TextEditingController emailController=TextEditingController();
+  TextEditingController passwordController=TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     final height=MediaQuery.sizeOf(context).height;
@@ -19,7 +24,20 @@ class _SignUpViewState extends State<SignUpView> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
         CustomText(text: 'Sign Up'),
-
+          CustomTextFormField(
+              hintText: 'Enter your email',
+              lableText: 'Enter your email',
+              controller: emailController
+          ),
+          CustomTextFormField(
+              hintText: 'Enter your Password',
+              lableText: 'Enter your Password',
+              controller: passwordController
+          ),
+          ElevatedButton(
+              onPressed: (){},
+              child: CustomText(text: 'Sign Up'),
+          )
       ],),
     );
   }
