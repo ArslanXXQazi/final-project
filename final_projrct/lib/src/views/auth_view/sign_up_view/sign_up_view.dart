d16c8e74524f6b47a37fffd6361847f91cc217e9
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-06 12:07:13 +0500

    Update sign_up_view.dart

--- final_projrct/lib/src/views/auth_view/sign_up_view/sign_up_view.dart
+++ final_projrct/lib/src/views/auth_view/sign_up_view/sign_up_view.dart
@@ -19,28 +19,38 @@ class _SignUpViewState extends State<SignUpView> {
     final height=MediaQuery.sizeOf(context).height;
     final width=MediaQuery.sizeOf(context).width;
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-        CustomText(text: 'Sign Up'),
-          CustomTextFormField(
-              hintText: 'Enter your email',
-              lableText: 'Enter your email',
-              controller: emailController
-          ),
-          CustomTextFormField(
-              hintText: 'Enter your Password',
-              lableText: 'Enter your Password',
-              controller: passwordController
-          ),
-          Center(
-            child: ElevatedButton(
-                onPressed: (){},
-                child: CustomText(text: 'Sign Up'),
+      body: Padding(
+        padding:  EdgeInsets.symmetric(horizontal: width*.03),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          CustomText(text: 'Sign Up'),
+            SizedBox(height: height*.02,),
+            CustomTextFormField(
+                hintText: 'Enter your email',
+                lableText: 'Enter your email',
+                controller: emailController
             ),
-          )
-      ],),
+            SizedBox(height: height*.02,),
+            CustomTextFormField(
+                hintText: 'Enter your Password',
+                lableText: 'Enter your Password',
+                controller: passwordController
+            ),
+            SizedBox(height: height*.02,),
+            Center(
+              child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange,
+                    minimumSize: Size(width*1, height*.07),
+                  ),
+                  child: CustomText(text: 'Sign Up'),
+              ),
+            )
+        ],),
+      ),
     );
   }
 }
