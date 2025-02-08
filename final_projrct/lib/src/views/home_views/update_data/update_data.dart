db7aee0f09ec1cbdde988abab471afb48b516742
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-08 14:50:19 +0500

    u

---
+++ final_projrct/lib/src/views/home_views/update_data/update_data.dart
@@ -0,0 +1,85 @@
+import 'package:final_projrct/src/controller/components/custom_Text.dart';
+import 'package:final_projrct/src/controller/components/custom_button.dart';
+import 'package:final_projrct/src/controller/components/custom_text_from_field.dart';
+import 'package:final_projrct/src/dbhelper/dbhelper.dart';
+import 'package:final_projrct/src/model/main_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class UpdateData extends StatefulWidget {
+  const UpdateData({super.key});
+
+  @override
+  State<UpdateData> createState() => _UpdateDataState();
+}
+
+class _UpdateDataState extends State<UpdateData> {
+  TextEditingController questionController=TextEditingController();
+
+  TextEditingController option1Controller=TextEditingController();
+
+  TextEditingController option2Controller=TextEditingController();
+
+  TextEditingController option3Controller=TextEditingController();
+
+  TextEditingController option4Controller=TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final height=MediaQuery.sizeOf(context).height;
+    final width=MediaQuery.sizeOf(context).width;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        leading: IconButton(onPressed: (){Navigator.pop(context);}, icon: Icon(Icons.arrow_back,color: Colors.white,)),
+        title: CustomText(
+          text: 'Insert Your Questions',
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      body: Padding(
+        padding:  EdgeInsets.symmetric(horizontal: width*.03,vertical: height*.02),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomTextFromField(
+                controller: questionController,
+                hintText: "Enter your question",
+                labelText: "Enter your question",
+              ),
+              CustomTextFromField(
+                controller: option1Controller,
+                hintText: "Enter your first option",
+                labelText: "Enter your first option",
+              ),
+              CustomTextFromField(
+                controller: option2Controller,
+                hintText: "Enter your second option",
+                labelText: "Enter your second option",
+              ),
+              CustomTextFromField(
+                controller: option3Controller,
+                hintText: "Enter your third option",
+                labelText: "Enter your third option",
+              ),
+              CustomTextFromField(
+                controller: option4Controller,
+                hintText: "Enter your fourth option",
+                labelText: "Enter your fourth option",
+              ),
+              CustomButton(
+                onTap: ()
+               {},
+                name: "Update Data",
+              ),
+
+            ],),
+        ),
+      ),
+    );
+  }
+}
