1b74cc355ff2d4ad3d3a2d9971a1e2d75e57bac4
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-12 21:01:45 +0500

    Update update_data.dart

--- final_projrct/lib/src/views/home_views/update_data/update_data.dart
+++ final_projrct/lib/src/views/home_views/update_data/update_data.dart
@@ -94,8 +94,7 @@ DbClass dbClass= DbClass.instance;
                 labelText: "Enter your question",
               ),
               CustomTextFromField(
-                controller: option1Controller,
-                hintText: "Enter your first option",
+          
                 labelText: "Enter your first option",
               ),
               CustomTextFromField(
