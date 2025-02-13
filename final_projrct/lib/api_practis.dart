324ecbf4db6a35fccec4e892c78240ef32a9ff91
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-13 15:41:00 +0500

    Update api_practis.dart

---
+++ final_projrct/lib/api_practis.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ApiPractis extends StatefulWidget {
+  const ApiPractis({super.key});
+
+  @override
+  State<ApiPractis> createState() => _ApiPractisState();
+}
+
+class _ApiPractisState extends State<ApiPractis> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+
+      ],),
+    );
+  }
+}
