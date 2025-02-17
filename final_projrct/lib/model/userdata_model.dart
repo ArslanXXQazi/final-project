d18ed00ba2c42327e24bb0b5d4a449717ba7438d
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-17 12:15:15 +0500

    update

--- final_projrct/lib/model/userdata_model.dart
+++ final_projrct/lib/model/userdata_model.dart
@@ -1,31 +1,27 @@
-class UserModel {
-   int? id;
-   String ?firstName;
-   String ?lastName;
-   String ?email;
-   String ?avatar;
+class UserModel
+{
+  int id;
+  String firstName;
+  String email;
+  String lastName;
+  String avatar;
 
   UserModel({
     required this.id,
     required this.firstName,
     required this.lastName,
     required this.email,
-    required this.avatar,
-  });
+    required this.avatar
+});
 
-  static UserModel fromMap(Map map) {
+  static UserModel fromMap(Map map){
     return UserModel(
         id: map['id'],
         firstName: map['first_name'],
-        lastName: map['last_ame'],
-        email: map['emil'],
+        lastName: map['last_name'],
+        email: map['email'],
         avatar: map['avatar']);
-    //   UserModel(
-    //   id: map['id'],
-    //   firstName: map['first_name'],
-    //   lastName: map['last_name'],
-    //   email: map['email'],
-    //   avatar: map['avatar'],
-    // );
   }
+
+
 }
