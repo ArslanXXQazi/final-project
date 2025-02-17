db6460a5c136e5243d266e6c9ea82fbd0af82683
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-17 12:50:46 +0500

    Update api_practis.dart

--- final_projrct/lib/api_practis.dart
+++ final_projrct/lib/api_practis.dart
@@ -99,6 +99,7 @@ import 'package:final_projrct/model/userdata_model.dart';
 import 'package:final_projrct/src/controller/components/custom_Text.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:path/path.dart';
 
 class ApiPractis extends StatefulWidget {
   const ApiPractis({super.key});
@@ -111,6 +112,25 @@ class _ApiPractisState extends State<ApiPractis> {
   @override
 
   List<UserModel> userModel=[];
+
+  Future<void> fetchData()
+  async{
+    final url =Uri.parse("https://reqres.in/api/users?page=2");
+    final response= await http.get(url);
+    if(response.statusCode==200)
+      {
+        final data=response.body;
+        setState(() {
+          
+        });
+      }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
 
   Widget build(BuildContext context) {
     return const Placeholder();
