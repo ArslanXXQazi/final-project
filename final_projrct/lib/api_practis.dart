810c062024f96246cbd91cdf2f8a09bf3c0b77e1
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-17 12:19:48 +0500

    Update api_practis.dart

--- final_projrct/lib/api_practis.dart
+++ final_projrct/lib/api_practis.dart
@@ -93,3 +93,27 @@
 //
 //
 
+import 'dart:convert';
+
+import 'package:final_projrct/model/userdata_model.dart';
+import 'package:final_projrct/src/controller/components/custom_Text.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class ApiPractis extends StatefulWidget {
+  const ApiPractis({super.key});
+
+  @override
+  State<ApiPractis> createState() => _ApiPractisState();
+}
+
+class _ApiPractisState extends State<ApiPractis> {
+  @override
+
+  List<UserModel> userModel=[];
+
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
